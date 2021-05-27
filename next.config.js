7205372d428fe1f82a7987acb5c93310062b2751
Aravind Balla <bsaaravind@gmail.com>
2021-05-27 16:23:16 +0530

    feat: proxy plausible

--- next.config.js
+++ next.config.js
@@ -44,4 +44,16 @@ module.exports = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: '/js/script.js',
+        destination: 'https://plausible.io/js/plausible.js',
+      },
+      {
+        source: '/api/event',
+        destination: 'https://plausible.io/api/event',
+      },
+    ];
+  },
 };
