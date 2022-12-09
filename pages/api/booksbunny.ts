03eae7ba07c51f21c944a19720a4d68b38b44387
Aravind Balla <bsaaravind@gmail.com>
2022-12-09 16:29:11 +0530

    fix: return data in the hangler

--- pages/api/booksbunny.ts
+++ pages/api/booksbunny.ts
@@ -1,20 +1,22 @@
-// import applyRateLimit from 'lib/rate-limit';
+import applyRateLimit from 'lib/rate-limit';
 import { NextRequest } from 'next/server';
 
 const API = 'https://askbrain.balla.dev/api';
 
 export default async function booksBunny(req: NextRequest, res) {
-  // try {
-  //   await applyRateLimit(req, res);
-  // } catch {
-  //   return res.status(429).send('Too Many Requests');
-  // }
+  try {
+    await applyRateLimit(req, res);
+  } catch {
+    return res.status(429).send('Too Many Requests');
+  }
 
   if (req.method === 'POST') {
     // @ts-ignore
     const query = req.body.query;
     const response = await fetch(`${API}?q=${encodeURI(query)}`);
     const data = await response.json();
+
+    res.json(data);
   } else {
     res.status(404).send();
   }
